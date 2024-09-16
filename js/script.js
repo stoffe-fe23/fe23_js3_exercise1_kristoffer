7cd34c75b47db5fc8679f4224715f207588e5a98
stoffe-fe23 <kristoffer.bengtsson@gritacademy.se>
2024-09-16 15:56:03 +0200

    wip

--- js/script.js
+++ js/script.js
@@ -2,20 +2,38 @@
 // Front page is loaded by default
 loadPage("home");
 
+
 // Load page content when navigating browser history
-window.addEventListener('popstate', (event) => {
+window.addEventListener("popstate", (event) => {
     loadPage(event.state.page);
 });
 
+// Load specific sub-page if URL contains a page parameter
+window.addEventListener("load", (event) => {
+    const page = new URLSearchParams(window.location.search).get("page");
+    if (page && page.length) {
+        loadPage(page);
+    }
+});
+
 // Menu bar click events
-document.querySelector("nav").addEventListener("click", (event) => {
-    history.pushState({ page: event.target.dataset.destination }, '', event.target.dataset.destination);
-    loadPage(event.target.dataset.destination);
+document.querySelector("nav > ul").addEventListener("click", (event) => {
+    if ((event.target.tagName == "LI") && (event.target.dataset.destination !== undefined)) {
+        history.pushState({ page: event.target.dataset.destination }, '', event.target.dataset.destination);
+        loadPage(event.target.dataset.destination);
+    }
 });
 
 // Load sub-page content of the specified page
 async function loadPage(pagename) {
-    const result = await fetch(`parts/${pagename}.html`);
-    const response = await result.text();
-    document.querySelector("#content").innerHTML = response;
+    try {
+        if (pagename && pagename.length) {
+            const result = await fetch(`parts/${pagename}.html`);
+            const response = await result.text();
+            document.querySelector("#content").innerHTML = response;
+        }
+    }
+    catch (error) {
+        console.error("Error loading page fragment", error);
+    }
 }
